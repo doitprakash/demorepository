1f1e33c2434b6a4c666671e5d6a4abcec1ed8aa8
prakash <prakashranjantripathi@gmail.com>
2020-12-31 23:55:16 +0530

    three commit

--- BankManagementSystem/src/com/braindata/bankmanagement/client/Test.java
+++ BankManagementSystem/src/com/braindata/bankmanagement/client/Test.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Test {
          
 	public static void main(String[] args){
+System.out.println ("new line");
 		Sbi bank=new Sbi();
 		while(true) {
 			System.out.println("For Creating Account         Press 1\n"+
